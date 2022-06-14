4c78461b1fb9d34594a31f0b26a69f1332fed957
Daniel <103138876+danazo77@users.noreply.github.com>
2022-06-13 19:35:03 -0600

    Add files via upload

---
+++ botones.js
@@ -0,0 +1,17 @@
+    // botones de la pagina index.
+    let iniciar = document.getElementById("#boton");
+    iniciar.addEventListener("click", _ => {
+        javascript:window.location.reload();;
+})
+
+let DesistirJuego = document.querySelector(".desistir");
+    function DesistirDelJuego(){
+        onclick.window.location.href="home.html";
+}
+
+//botones pagina inicio
+
+let IniciarJuego = document.querySelector(".juego");
+    function InicioJuego(){
+        onclick.window.location.href="index.html"
+    }
